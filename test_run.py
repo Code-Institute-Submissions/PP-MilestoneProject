8033441987f8bde693a7843ef12abea6df8edc04
Colman Leung <comacoma0218@hotmail.com>
2018-06-20 14:19:36 +0000

    Added test_run.py (automated test suite for run.py)

---
+++ test_run.py
@@ -0,0 +1,17 @@
+from run import app
+import unittest
+
+class testSuite(unittest.TestCase):
+    """
+    Test suite for this project
+    """
+    
+    #To test if Flask is set up correctly
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type="html/text")
+        self.assertEqual(response.status_code, 200)
+        
+
+if __name__ == '__main__':
+    unittest.main()
