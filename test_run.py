b4cc88ae7bbe3bd796ae89132daebaeecb0497c8
Colman Leung <comacoma0218@hotmail.com>
2018-06-23 13:30:00 +0000

    Categorize tests into different class

--- test_run.py
+++ test_run.py
@@ -1,39 +1,61 @@
 from run import *
 import unittest
 
-class testSuite(unittest.TestCase):
+class testRoute(unittest.TestCase):
     """
-    Test suite for this project
+    Test class for routing
     """
-    
     #To test if Flask is set up correctly
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type="html/text")
         self.assertEqual(response.status_code, 200)
         
-    #To test if the page loads correctly (index.html)
+     #To test if the page loads correctly (index.html)
     def test_index_loads(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type="html/text")
         self.assertTrue(b'Riddle Me These' in response.data)
-    
-    #To test if data is written to player.json as expected
-    def test_write_to_player_json(self):
-        with open('data/dummy_player.json', 'w') as f:
-            json.dump([], f)
-        for x in range(5):
-            write_to_player_json('dummy_player', 'data/dummy_player.json')
-        with open('data/dummy_player.json', 'r') as f:
-            data = json.load(f)
-            self.assertEqual(len(data), 5)
-    
+        
     #To test if page loads correctly (player.html)
     def test_player_loads(self):
         tester = app.test_client(self)
         response = tester.get('/dummy_player')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'Welcome, dummy_player' in response.data)
+        self.assertTrue(b'Welcome, dummy_player(guest)' in response.data)
+
+
+class testSuite(unittest.TestCase):
+    """
+    Test suite for this project
+    """
+    
+    #To set up player.json before each tests
+    def setUp(self):
+        with open('data/player.json', 'w') as f:
+            json.dump([], f)
+            
+    #To remove all data from player.json after all tests
+    @classmethod
+    def tearDownClass(cls):
+        with open('data/player.json', 'w') as f:
+            json.dump([], f)
+    
+    #To test if data is written to player.json as expected
+    def test_write_to_player_json(self):
+        for x in range(5):
+            write_to_player_json('dummy_player', 'data/player.json')
+        with open('data/player.json', 'r') as f:
+            data = json.load(f)
+        self.assertEqual(len(data), 5)
+            
+        #To test if data is read from player.json as expected
+    def test_read_player_json(self):
+        expected_data = [{'player_name': 'dummy_player', 'score': 0}]
+        write_to_player_json('dummy_player', 'data/player.json')
+        with open('data/player.json', 'r') as f:
+            data = json.load(f)
+        self.assertEqual(data, expected_data)
         
     #To test if player login behaves as expected
     def test_login(self):
@@ -44,8 +66,9 @@ class testSuite(unittest.TestCase):
         
     #To test if application can identify both existing and new player
     def test_identify_player(self):
-        self.assertTrue(is_new_player('dummy_player', 'data/dummy_player.json'))
-        self.assertFalse(is_new_player('player', 'data/dummy_player.json'))
+        write_to_player_json('dummy_player', 'data/player.json')
+        self.assertFalse(is_new_player('dummy_player', 'data/player.json'))
+        self.assertTrue(is_new_player('player', 'data/player.json'))
     
 if __name__ == '__main__':
     unittest.main()
