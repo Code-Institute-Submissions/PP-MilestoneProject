787abe93d8640eb640d6f9f55b1bb0e7aa29f21c
Colman Leung <comacoma0218@hotmail.com>
2018-06-23 13:30:42 +0000

    Implemented template output for player.html

--- run.py
+++ run.py
@@ -4,43 +4,53 @@ from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+def read_player_json(file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return data
+
 def write_to_player_json(player_name, file_name):
     player = {"player_name": player_name, "score": 0}
-    with open(file_name) as f:
-        data = json.load(f)
+    players =  players = read_player_json('data/player.json')
     
-    data.append(player)
+    players.append(player)
     
     with open(file_name, 'w') as f:
-        json.dump(data, f)
+        json.dump(players, f)
+        
+def zero_player(file_name):
+    players = read_player_json('data/player.json')
+    return len(players) == 0
 
 def is_new_player(player_name, file_name):
     with open(file_name, 'r') as f:
         players = json.load(f)
+        
     for p in players:
         if p['player_name'] == player_name:
-            return True
-        else:
             return False
+        else:
+            return True
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     #Handle POST requests
     if request.method == "POST":
-        if is_new_player(request.form['player_name'], 'data/player.json'):
+        if is_new_player(request.form['player_name'], 'data/player.json') or zero_player('data/player.json'):
             write_to_player_json(request.form["player_name"], 'data/player.json')
         return redirect(request.form["player_name"])
     return render_template("index.html")
     
 @app.route('/<player_name>')
 def player(player_name):
-    player_detail = {}
-    with open('data/player.json', 'r') as f:
-        players = json.load(f)
-        for player in players:
-            if player["player_name"] == player_name:
-                player_detail = player
-    return render_template("player.html", player=player)
+    player_detail = {"player_name": player_name + "(guest)", "score": "place_holder"}
+    if not zero_player('data/player.json'):
+        players = read_player_json('data/player.json')
+        for p in players:
+            if p["player_name"] == player_name:
+                player_detail = p
+        return render_template("player.html", player=p)
+    return render_template("player.html", player=player_detail) # Return template output if read failed.
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
