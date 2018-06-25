be4ac2a0d87cdb816ccfed001cf71d8914e56bd8
Colman Leung <comacoma0218@hotmail.com>
2018-06-25 01:43:14 +0000

    Functionality of riddles processing implemented in full

--- run.py
+++ run.py
@@ -1,28 +1,29 @@
 import os
 import json
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 import random
 
 app = Flask(__name__)
 
-def read_player_json(file_name):
+def read_json(file_name):
     with open(file_name, 'r') as f:
         data = json.load(f)
     return data
     
 def read_player_detail(player_name, file_name):
     if zero_player(file_name):
-        return {"player_name": player_name + "(guest)", "score": "place_holder"}
+        return {"player_name": player_name, "score": 0}
     else:
-        players = read_player_json('data/player.json')
+        players = read_json('data/player.json')
         for p in players:
             if p["player_name"] == player_name:
-                player_detail = p
-                return player_detail
+                return p
+        # Return template data if player name is not found in file
+        return {"player_name": player_name, "score": 0}
 
-def write_to_player_json(player_name, file_name):
+def register_new_player(player_name, file_name):
     player = {"player_name": player_name, "score": 0}
-    players = read_player_json('data/player.json')
+    players = read_json('data/player.json')
     
     players.append(player)
     
@@ -30,43 +31,108 @@ def write_to_player_json(player_name, file_name):
         json.dump(players, f)
         
 def zero_player(file_name):
-    players = read_player_json('data/player.json')
+    players = read_json('data/player.json')
     return len(players) == 0
 
 def is_new_player(player_name, file_name):
-    with open(file_name, 'r') as f:
-        players = json.load(f)
+    players = read_json('data/player.json')
         
     for p in players:
         if p['player_name'] == player_name:
             return False
         else:
             return True
-            
-def get_riddle(file_name):
+    
+def get_riddleID(file_name):
+    data = read_json('data/riddles.json')
+    return "Q" + str(random.randint(1, len(data)))
+    
+def get_riddle(file_name, ID):
+    data = read_json('data/riddles.json')
+    for d in data:
+        if d["ID"] == ID:
+            return d
+
+def pass_riddle(answer):
+    return len(answer) == 0
+
+def correct_answer(riddle, answer):
+    return riddle["A"] == answer.capitalize()
+    
+def update_score(player_name, file_name):
+    data = read_json(file_name)
+    for d in data:
+        if d["player_name"] == player_name:
+            d["score"] += 1
+            with open(file_name, 'w') as f:
+                json.dump(data, f)
+            return
+
+def add_wrong_answer(file_name, answer):
+    with open(file_name, 'a') as f:
+        f.writelines(answer + "\n")
+        
+def get_wrong_answers(file_name):
+    wa = []
     with open(file_name, 'r') as f:
-        data = json.load(f)
-    return random.choice(data)
+        wa = f.readlines()
+    return wa
+    
+def clean_wrong_answers(player_name):
+    file_name = "data/" + player_name + "_wa.txt"
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+    else:
+        return
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     #Handle POST requests
     if request.method == "POST":
         if is_new_player(request.form['player_name'], 'data/player.json') or zero_player('data/player.json'):
-            write_to_player_json(request.form["player_name"], 'data/player.json')
+            register_new_player(request.form["player_name"], 'data/player.json')
         return redirect(request.form["player_name"])
     return render_template("index.html")
     
 @app.route('/<player_name>')
 def player(player_name):
+    clean_wrong_answers(player_name)
     player_detail = read_player_detail(player_name, 'data/player.json')
     return render_template("player.html", player=player_detail) # Return template output if read failed.
     
-@app.route('/<player_name>/riddles')
+@app.route('/<player_name>/riddles', methods=['GET', 'POST'])
 def riddles(player_name):
+    riddleID = get_riddleID('data/riddles.json')
+    return redirect(url_for('riddle', player_name=player_name, riddleID=riddleID))
+    
+@app.route('/<player_name>/riddles/<riddleID>', methods=['GET', 'POST'])
+def riddle(player_name, riddleID):
     player_detail = read_player_detail(player_name, 'data/player.json')
-    riddle = get_riddle('data/riddles.json')
-    return render_template("riddles.html", player=player_detail, riddle=riddle)
+    riddle = get_riddle('data/riddles.json', riddleID)
+    file_name = "data/" + player_name + "_wa.txt"
+    if request.method == "POST":
+        if pass_riddle(request.form["answer"]):
+            clean_wrong_answers(player_name)
+            return redirect(url_for('riddles', player_name=player_name))
+        else:
+            return redirect(url_for('answer', player_name=player_name, riddleID=riddleID, answer=request.form["answer"]))
+    if os.path.isfile(file_name):
+        wa = get_wrong_answers(file_name)
+        return render_template("riddles.html", player=player_detail, riddle=riddle, wa=wa)
+    else:
+        return render_template("riddles.html", player=player_detail, riddle=riddle)
+
+@app.route('/<player_name>/riddles/<riddleID>/<answer>', methods=['GET', 'POST'])
+def answer(player_name, riddleID, answer):
+    riddle = get_riddle('data/riddles.json', riddleID)
+    if correct_answer(riddle, answer):
+        update_score(player_name, 'data/player.json')
+        clean_wrong_answers(player_name)
+        return redirect(url_for('riddles', player_name=player_name))
+    else:
+        file_name = "data/" + player_name + "_wa.txt"
+        add_wrong_answer(file_name, answer)
+        return redirect(url_for('riddle', player_name=player_name, riddleID=riddleID))
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
--- test_run.py
+++ test_run.py
@@ -27,14 +27,14 @@ class testRoute(unittest.TestCase):
         tester = app.test_client(self)
         response = tester.get('/dummy_player')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'Welcome, dummy_player(guest)' in response.data)
+        self.assertTrue(b'Welcome, dummy_player' in response.data)
         
     #To test if page loads correctly (riddles.html)
     def test_riddles_loads(self):
         tester = app.test_client(self)
-        response = tester.get('/dummy_player/riddles')
+        response = tester.get('/dummy_player/riddles', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'Welcome, dummy_player(guest)' in response.data)
+        self.assertTrue(b'Welcome, dummy_player' in response.data)
         self.assertTrue(b'Your challenges begins,' in response.data)
 
 
@@ -43,27 +43,27 @@ class testPlayerJson(unittest.TestCase):
     Test class related to data manipulation with player.json
     """
     
-    #To set up player.json before each tests
+    #To set up player.json before each tests in this class
     def setUp(self):
         reset_json('data/player.json')
             
-    #To remove all data from player.json after all tests
+    #To remove all data from player.json after all tests in this class
     @classmethod
     def tearDownClass(cls):
         reset_json('data/player.json')
     
     #To test if data is written to player.json as expected
-    def test_write_to_player_json(self):
+    def test_register_new_player(self):
         for x in range(5):
-            write_to_player_json('dummy_player', 'data/player.json')
-        data = read_player_json('data/player.json')
+            register_new_player('dummy_player', 'data/player.json')
+        data = read_json('data/player.json')
         self.assertEqual(len(data), 5)
             
     #To test if data is read from player.json as expected
-    def test_read_player_json(self):
+    def test_read_json(self):
         expected_data = [{'player_name': 'dummy_player', 'score': 0}]
-        write_to_player_json('dummy_player', 'data/player.json')
-        data = read_player_json('data/player.json')
+        register_new_player('dummy_player', 'data/player.json')
+        data = read_json('data/player.json')
         self.assertEqual(data, expected_data)
         
     #To test if player login form behaves as expected
@@ -75,7 +75,7 @@ class testPlayerJson(unittest.TestCase):
         
     #To test if application can identify both existing and new player
     def test_identify_player(self):
-        write_to_player_json('dummy_player', 'data/player.json')
+        register_new_player('dummy_player', 'data/player.json')
         self.assertFalse(is_new_player('dummy_player', 'data/player.json'))
         self.assertTrue(is_new_player('player', 'data/player.json'))
         
@@ -87,23 +87,46 @@ class testPlayerJson(unittest.TestCase):
         tester = app.test_client(self)
         for x in range(5):
             tester.post('/', data=dict(player_name = 'dummy_player'), follow_redirects = False)
-        data = read_player_json('data/player.json')
+        data = read_json('data/player.json')
         self.assertEqual(len(data), 1)
-    
-    #To test if read_player_detail behaves as expected
-    def test_read_player_detail(self):
-        data = read_player_detail('dummy_player', 'data/player.json')
-        self.assertTrue("(guest)" in data['player_name'])
-        write_to_player_json("dummy_player", 'data/player.json')
-        data = read_player_detail('dummy_player', 'data/player.json')
-        self.assertFalse("(guest)" in data['player_name'])
 
 class testRiddles(unittest.TestCase):
     """
     Test class related to retrieving, displaying, and processing riddles
     """
     
+     #To set up player.json before each tests in this class
+    def setUp(self):
+        reset_json('data/player.json')
+            
+    #To remove all data after all tests in this class
+    @classmethod
+    def tearDownClass(cls):
+        reset_json('data/player.json')
     
+    #To test if application can tell if player submitted a correct answer or not
+    def test_answer_riddle(self):
+        riddle = get_riddle('data/riddles.json', "Q1")
+        self.assertTrue(correct_answer(riddle, "ice"))
+        self.assertFalse(correct_answer(riddle, "something else"))
+    
+    #To test if scores are updated correctly
+    def test_update_score(self):
+        tester = app.test_client(self)
+        tester.post('/', data=dict(player_name = 'dummy_player'), follow_redirects = False)
+        tester.post('/', data=dict(player_name = 'dummy_player2'), follow_redirects = False)
+        tester.get('dummy_player/riddles/Q1/ice')
+        data = read_json('data/player.json')
+        self.assertEqual(data[0]["score"], 1)
+        self.assertEqual(data[1]["score"], 0)
+        
+    #To test if wrong answers are displayed properly
+    def test_display_wrong_answers(self):
+        tester = app.test_client(self)
+        tester.post('/', data=dict(player_name = 'dummy_player'), follow_redirects = False)
+        response = tester.get('/dummy_player/riddles/Q1/something', follow_redirects=True)
+        self.assertTrue(b"Here are the wrong answers you have entered so far:" in response.data)
+        self.assertTrue(b"something" in response.data)
     
 if __name__ == '__main__':
     unittest.main()
